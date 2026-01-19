0b1fafe526cfc5ab87560d2ccb342652aa0f07a8
William <willdev2025@outlook.com>
2026-01-19 11:41:44 -0800

    Made src/main.cc more like overengineered cuz im good at that :3

--- src/main.cc
+++ src/main.cc
@@ -2,17 +2,38 @@
 #include <printr.h>
 #include <filesystem>
 #include <fstream>
-#include "__winapi/defs.h"
 
 #include <elf.h>
 #include <mach-o/loader.h>
 #include <windows.h>
 
-uint32_t __bswap32(uint32_t x) {
-    return (x >> 24) | ((x >> 8) & 0xff00) | ((x << 8) & 0xff0000) | (x << 24);
-}
+template<int SZ>
+struct check_magic {
+    check_magic(int x, int mag) {
+        if (SZ == 16) {
+            auto v = (uint16_t)x;
+            if (v == mag || swap16(v) == mag) {
+                good = true;
+            }
+        } else if (SZ == 32) {
+            auto v = (uint32_t)x;
+            if (v == mag || swap32(v) == mag) {
+                good = true;
+            }
+        } else {
+            good = false;
+        }
+    }
 
-uint16_t __bswap16(uint16_t x) { return (x << 8) | (x >> 8); }
+    bool good;
+
+  private:
+    uint16_t swap16(uint16_t x) { return (x << 8) | (x >> 8); }
+    uint32_t swap32(uint32_t x) {
+        return (x >> 24) | ((x >> 8) & 0xff00) | ((x << 8) & 0xff0000) |
+               (x << 24);
+    }
+};
 
 int main(int ac, char** av) {
     if (ac != 2) {
@@ -35,20 +56,19 @@ int main(int ac, char** av) {
 
     int ret;
 
-    auto sig = (uint32_t*)data;
+    auto sig = (uint32_t)*data;
 
-    if (*sig == MH_MAGIC_64 || __bswap32(*sig) == MH_MAGIC_64) {
+    if (check_magic<32>(sig, MH_MAGIC_64).good) {
         printer::println("Mach-O File");
         ret = lsbin_machomain(data);
-    } else if (*sig == 0x7f454c46 || __bswap32(*sig) == 0x7f454c46) {
+    } else if (check_magic<32>(sig, 0x7f454c46).good) {
         printer::println("ELF File");
         ret = lsbin_elfmain(data);
-    } else if ((uint16_t)*sig == IMAGE_DOS_SIGNATURE ||
-               __bswap16((uint16_t)*sig) == IMAGE_DOS_SIGNATURE) {
+    } else if (check_magic<16>(sig, IMAGE_DOS_SIGNATURE).good) {
         printer::println("PE File");
         ret = lsbin_pemain(data);
     } else {
-        printer::eprintln("Unknown file type: Got magic 0x{:x}", *sig);
+        printer::eprintln("Unknown file type: Got magic 0x{:x}", sig);
         ret = 1;
     }
 
